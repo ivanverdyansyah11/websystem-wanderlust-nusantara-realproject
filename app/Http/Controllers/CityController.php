d8e7b9b183a611d61d697b179e4d74fffd3d420c
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-06-21 18:44:31 +0800

    Progress: CRUD City Page

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class CityController extends Controller
@@ -28,5 +29,67 @@ class CityController extends Controller
             'text_history_1' => $text_history_1,
             'text_history_2' => $text_history_2,
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'image' => 'file|image',
+            'history_1' => 'required',
+            'history_2' => 'required',
+        ]);
+
+        $validatedData['image'] = $request->file('image')->store('city-images');
+        $city = City::create($validatedData);
+        if ($city) {
+            return redirect(route('index-city'))->with('success', 'Add New City Successfully!');
+        } else {
+            return redirect(route('index-city'))->with('failed', 'Add New City Failed!');
+        }
+    }
+
+    public function edit($id)
+    {
+        $city = City::where('id', $id)->first();
+        return response()->json($city);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'image' => 'file|image',
+            'history_1' => 'required',
+            'history_2' => 'required',
+        ]);
+
+        if ($request->file('image')) {
+            Storage::delete($request->oldImage);
+            $validatedData['image'] = $request->file('image')->store('city-images');
+        }
+
+        $city = City::findOrFail($id)->update($validatedData);
+        if ($city) {
+            return redirect(route('index-city'))->with('success', 'Update City Successfully!');
+        } else {
+            return redirect(route('index-city'))->with('failed', 'Update City Failed!');
+        }
+    }
+
+    public function delete($id)
+    {
+        $city = City::findOrFail($id);
+
+        if ($city->image) {
+            Storage::delete($city->image);
+        }
+
+        $city = $city->delete();
+        if ($city) {
+            return redirect(route('index-city'))->with('success', 'Delete City Successfully!');
+        } else {
+            return redirect(route('index-city'))->with('failed', 'Delete City Failed!');
+        }
     }
 }
--- resources/views/city/index.blade.php
+++ resources/views/city/index.blade.php
@@ -1,25 +1,25 @@
 @extends('templates.main')
 
 @section('main')
-    @if (session()->has('success'))
-        <div class="alert alert-success mx-5 mb-4" role="alert">
-            {{ session('success') }}
-        </div>
-    @elseif(session()->has('failed'))
-        <div class="alert alert-danger mx-5 mb-4" role="alert">
-            {{ session('failed') }}
-        </div>
-    @endif
-
     <div class="header-section d-flex flex-row justify-content-between padding-section px-5">
 
         <p class="text-black fw-medium fs-2">{{ $page }} Page</p>
-        <button class="btn btn-color d-lg-flex d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">Add New
+        <button class="btn btn-color d-lg-flex d-none" data-bs-toggle="modal" data-bs-target="#AddModal">Add New
             City</button>
         <div class="d-xl-none hamburger-wrapper d-flex text-white align-self-center">
             <i class="fa-solid fa-bars"></i>
         </div>
     </div>
+
+    @if (session()->has('success'))
+        <div class="alert alert-success mx-5 my-4" role="alert">
+            {{ session('success') }}
+        </div>
+    @elseif(session()->has('failed'))
+        <div class="alert alert-danger mx-5 my-4" role="alert">
+            {{ session('failed') }}
+        </div>
+    @endif
 
     <div class="main-content mt-4 px-5 mb-5">
         <div class="row px-3 py-4 rounded-1" style="background-color: white">
@@ -70,33 +70,24 @@
         </div>
     </div>
 
-    {{-- <form action="{{ route('store-menu') }}" method="post" enctype="multipart/form-data">
+    <form action="{{ route('store-city') }}" method="post" enctype="multipart/form-data">
         @csrf
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="AddModal" tabindex="-1" aria-labelledby="AddModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-body p-5">
-                        <div class="text-center fs-3 title-font fw-medium">Add New Menu</div>
-                        <div class="d-flex flex-column gap-3">
+                        <div class="text-center fs-3 title-font fw-medium">Add New City</div>
+                        <div class="d-flex flex-column">
                             <div class="pt-2 w-100">
-                                <input type="hidden" name="id" value="{{ $id + 1 }}">
-                                <div class="input-text-wrapper w-100 mt-2">
-                                    <p class="text-black fw-medium fs-14">Name</p>
-                                    <input type="text" name="name"
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <label for="name" class="text-black fw-medium fs-14">Name</label>
+                                    <input type="text" id="name" name="name"
                                         class="w-100 input-text border-0 @error('name') is-invalid @enderror"
-                                        placeholder="Enter menu name" value="{{ old('name') }}">
+                                        placeholder="Enter city name" value="{{ old('name') }}">
                                 </div>
                             </div>
                             <div class="w-100">
-                                <div class="input-text-wrapper w-100 mt-2">
-                                    <p class="text-black fw-medium fs-14">Price</p>
-                                    <input type="text" name="price"
-                                        class="w-100 input-text border-0 @error('price') is-invalid @enderror"
-                                        placeholder="Enter menu price" value="{{ old('price') }}">
-                                </div>
-                            </div>
-                            <div class="w-100">
-                                <div class="input-text-wrapper w-100 mt-2">
+                                <div class="input-text-wrapper w-100 mb-3">
                                     <p class="text-black fw-medium fs-14">Image</p>
                                     <div class="d-flex flex-row align-items-end gap-2">
                                         <img src="{{ asset('assets/img/table/img-modal.svg') }}" alt="your image"
@@ -107,16 +98,25 @@
                                 </div>
                             </div>
                             <div class="w-100">
-                                <div class="input-text-wrapper w-100 mt-2">
-                                    <p class="text-black fw-medium fs-14">Description</p>
-                                    <textarea type="text" name="description" class="w-100 input-text border-0 @error('description') is-invalid @enderror"
-                                        placeholder="Enter menu description" value="{{ old('description') }}"></textarea>
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <label for="history_1" class="text-black fw-medium fs-14">History Paragraph 1</label>
+                                    <textarea type="text" id="history_1" name="history_1"
+                                        class="w-100 input-text border-0 @error('history_1') is-invalid @enderror"
+                                        placeholder="Enter city history paragraph 1" value="{{ old('history_1') }}" rows="3"></textarea>
+                                </div>
+                            </div>
+                            <div class="w-100">
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <label for="history_2" class="text-black fw-medium fs-14">History Paragraph 2</label>
+                                    <textarea type="text" id="history_2" name="history_2"
+                                        class="w-100 input-text border-0 @error('history_2') is-invalid @enderror"
+                                        placeholder="Enter city history paragraph 2" value="{{ old('history_2') }}" rows="3"></textarea>
                                 </div>
                             </div>
                         </div>
                         <div class="d-flex flex-row justify-content-center gap-2 pt-4">
                             <button type="button" class="btn btn-dark fs-15" data-bs-dismiss="modal">Cancel Add</button>
-                            <button type="submit" class="btn btn-color fs-15">Add New Menu</button>
+                            <button type="submit" class="btn btn-color fs-15">Add New City</button>
                         </div>
                     </div>
                 </div>
@@ -133,39 +133,37 @@
                         <div class="text-center fs-3 title-font fw-medium fw-medium">Edit Menu</div>
                         <div class="d-flex flex-column gap-3">
                             <div class="pt-2 w-100">
-                                <div class="input-text-wrapper w-100 mt-2">
-                                    <p class="text-black fs-14 fw-medium">Name</p>
-                                    <input type="text" name="name" class="w-100 input-text border-0"
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <label for="name" class="text-black fw-medium fs-14">Name</label>
+                                    <input type="text" id="name" name="name"
+                                        class="w-100 input-text border-0 @error('name') is-invalid @enderror"
                                         data-value="name">
                                 </div>
                             </div>
                             <div class="w-100">
-                                <div class="input-text-wrapper w-100 mt-2">
-                                    <p class="text-black fs-14 fw-medium">Price</p>
-                                    <input type="text" name="price" class="w-100 input-text border-0"
-                                        data-value="price">
-                                </div>
-                            </div>
-                            <div class="w-100">
-                                <div class="input-text-wrapper w-100 mt-2">
+                                <div class="input-text-wrapper w-100 mb-3">
                                     <input type="hidden" name="oldImage" data-value="oldImage">
-
-                                    <p class="text-black fs-14 fw-medium">Image</p>
-
+                                    <p class="text-black fw-medium fs-14">Image</p>
                                     <div class="d-flex flex-row align-items-end gap-2">
-
-                                        <img src="" class="tag-image-edit img-fluid" alt="Input Image"
-                                            width="150" data-value="image">
-
+                                        <img src="" alt="your image" class="tag-image-edit img-preview"
+                                            data-value="image" />
                                         <input type='file' class="input-file-edit @error('image') is-invalid @enderror"
                                             size="150" name="image">
                                     </div>
                                 </div>
                             </div>
                             <div class="w-100">
-                                <div class="input-text-wrapper w-100 mt-2">
-                                    <p class="text-black fs-14 fw-medium">Description</p>
-                                    <textarea type="text" name="description" class="w-100 input-text border-0" data-value="description"></textarea>
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <label for="history_1" class="text-black fw-medium fs-14">History Paragraph 1</label>
+                                    <textarea type="text" id="history_1" name="history_1"
+                                        class="w-100 input-text border-0 @error('history_1') is-invalid @enderror" rows="3" data-value="history_1"></textarea>
+                                </div>
+                            </div>
+                            <div class="w-100">
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <label for="history_2" class="text-black fw-medium fs-14">History Paragraph 2</label>
+                                    <textarea type="text" id="history_2" name="history_2"
+                                        class="w-100 input-text border-0 @error('history_2') is-invalid @enderror" rows="3" data-value="history_1"></textarea>
                                 </div>
                             </div>
                         </div>
@@ -186,19 +184,19 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-body p-5">
-                        <div class="text-center fs-3 fw-medium title-font">Delete Menu</div>
-                        <div class="pt-3 text-center"> Do you really want to delete these menu? This process cannot be
+                        <div class="text-center fs-3 fw-medium title-font">Delete City</div>
+                        <div class="pt-3 text-center"> Do you really want to delete these city? This process cannot be
                             undone.
                         </div>
                         <div class="d-flex flex-row justify-content-center gap-2 pt-4">
                             <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Delete</button>
-                            <button type="submit" class="btn btn-color">Delete Menu</button>
+                            <button type="submit" class="btn btn-color">Delete City</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </form> --}}
+    </form>
 
     <script src="{{ asset('assets/js/jquery-3.5.1.js') }}"></script>
     <script src="https://kit.fontawesome.com/9e88c62f38.js" crossorigin="anonymous"></script>
@@ -207,16 +205,16 @@
     <script>
         $(document).on('click', '[data-bs-target="#EditModal"]', function() {
             let id = $(this).data('id');
-            $('#edit-modal').attr('action', '/admin/menu/edit/' + id);
+            $('#edit-modal').attr('action', '/admin/city/edit/' + id);
             $.ajax({
                 type: 'get',
-                url: '/admin/menu/edit/' + id,
+                url: '/admin/city/edit/' + id,
                 success: function(data) {
                     $('[data-value="name"]').val(data.name);
-                    $('[data-value="price"]').val(data.price);
                     $('[data-value="oldImage"]').val(data.image);
                     $('[data-value="image"]').attr("src", "/storage/" + data.image);
-                    $('[data-value="description"]').val(data.description);
+                    $('[data-value="history_1"]').val(data.history_1);
+                    $('[data-value="history_2"]').val(data.history_2);
 
                     console.log($('[data-value="image"]').attr("src"));
                 }
@@ -225,21 +223,13 @@
 
         $(document).on('click', '[data-bs-target="#DeleteModal"]', function() {
             let id = $(this).data('id');
-            $('#delete-modal').attr('action', '/admin/menu/delete/' + id);
+            $('#delete-modal').attr('action', '/admin/city/delete/' + id);
         });
 
-        const btnHamburger = document.querySelector('.fa-bars');
-        const sidebar = document.querySelector('.sidebar-wrapper');
-        const body = document.querySelector('body');
         const tagImage = document.querySelector('.tag-image');
         const inputImage = document.querySelector('.input-file');
         const tagImageEdit = document.querySelector('.tag-image-edit');
         const inputImageEdit = document.querySelector('.input-file-edit');
-
-        btnHamburger.addEventListener('click', function() {
-            btnHamburger.classList.toggle('active');
-            sidebar.classList.toggle('active');
-        });
 
         inputImage.addEventListener('change', function() {
             tagImage.src = URL.createObjectURL(inputImage.files[0]);
@@ -247,6 +237,15 @@
 
         inputImageEdit.addEventListener('change', function() {
             tagImageEdit.src = URL.createObjectURL(inputImageEdit.files[0]);
+        });
+
+        const btnHamburger = document.querySelector('.fa-bars');
+        const sidebar = document.querySelector('.sidebar-wrapper');
+        const body = document.querySelector('body');
+
+        btnHamburger.addEventListener('click', function() {
+            btnHamburger.classList.toggle('active');
+            sidebar.classList.toggle('active');
         });
     </script>
 @endsection
