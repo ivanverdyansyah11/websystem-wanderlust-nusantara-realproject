f550458be2355b79e4c1ea16159d7f1eb1fc2e60
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-08-13 15:42:21 +0800

    Progress: Index Feedback Dashboard Page

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Feedback;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class FeedbackController extends Controller
 {
-    //
+    public function index()
+    {
+        if (!$locale = session('locale')) {
+            Session::put('locale', 'id');
+        } else {
+            app()->setLocale($locale);
+        }
+
+        return view('feedback.index', [
+            'page' => 'Feedback',
+            'feedbacks' => Feedback::all(),
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $feedback = Feedback::where('id', $id)->first();
+        return response()->json($feedback);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'username' => 'required',
+            'position' => 'required',
+            'message' => 'required',
+        ]);
+
+        $feedback = Feedback::findOrFail($id)->update($validatedData);
+        if ($feedback) {
+            return redirect(route('index-feedback'))->with('success', 'Update Feedback Successfully!');
+        } else {
+            return redirect(route('index-feedback'))->with('failed', 'Update Feedback Failed!');
+        }
+    }
+
+    public function delete($id)
+    {
+        $feedback = Feedback::findOrFail($id);
+        $feedback = $feedback->delete();
+        if ($feedback) {
+            return redirect(route('index-feedback'))->with('success', 'Delete Feedback Successfully!');
+        } else {
+            return redirect(route('index-feedback'))->with('failed', 'Delete Feedback Failed!');
+        }
+    }
 }
---
+++ resources/views/feedback/index.blade.php
@@ -0,0 +1,228 @@
+@extends('templates.main')
+
+@section('main')
+    <div class="header-section d-flex flex-row justify-content-between padding-section px-5">
+        <p class="text-black fw-medium fs-2">Feedback Page</p>
+        <div class="d-lg-flex justify-content-end d-none gap-2">
+            <div class="d-xl-none hamburger-wrapper d-flex text-white align-self-center">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-dark dropdown-toggle" style="height: fit-content; padding: 13px 22px;" type="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ strtoupper(session('locale')) ?? strtoupper(config('app.locale')) }}
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ route('switch-language', ['locale' => 'id']) }}">ID</a>
+                    </li>
+                    <li><a class="dropdown-item" href="{{ route('switch-language', ['locale' => 'en']) }}">EN</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    @if (session()->has('success'))
+        <div class="alert alert-success mx-5 my-4" role="alert">
+            {{ session('success') }}
+        </div>
+    @elseif(session()->has('failed'))
+        <div class="alert alert-danger mx-5 my-4" role="alert">
+            {{ session('failed') }}
+        </div>
+    @endif
+
+    <div class="main-content mt-4 px-5 mb-5">
+        <div class="row px-3 py-4 rounded-1" style="background-color: white">
+            <div class="col-12 my-2">
+                <table class="table mb-0">
+                    <thead>
+                        <tr>
+                            <td>No</td>
+                            <td>Name</td>
+                            <td>Job Position</td>
+                            <td>Message</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($feedbacks->count() == 0)
+                            <tr>
+                                <td colspan="5" class="text-center py-3">Data Feedback Not Found!</td>
+                            </tr>
+                        @else
+                            @foreach ($feedbacks as $i => $feedback)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $feedback->username }}</td>
+                                    <td>{{ $feedback->position }}</td>
+                                    <td style="width: 300px">{{ $feedback->message }}</td>
+                                    <td class=" flex-row gap-1 d-lg-flex d-none">
+                                        <button class="btn btn-edit d-lg-flex d-none p-0" data-bs-toggle="modal"
+                                            data-bs-target="#EditModal" data-id="{{ $feedback->id }}">
+                                            <img src="{{ asset('assets/img/btn/edit-icon.svg') }}">
+                                        </button>
+                                        <button class="btn btn-edit p-0 d-lg-flex d-none" data-bs-toggle="modal"
+                                            data-bs-target="#DeleteModal" data-id="{{ $feedback->id }}">
+                                            <img src="{{ asset('assets/img/btn/delete-icon.svg') }}">
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    {{-- <form id="edit-modal" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="modal fade" id="EditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-body p-5">
+                        <div class="text-center fs-3 title-font fw-medium fw-medium">Edit Menu</div>
+                        <div class="d-flex flex-column gap-3">
+                            <div class="wrapper d-flex gap-3">
+                                <div class="pt-2 w-100">
+                                    <div class="input-text-wrapper w-100 mb-3">
+                                        <label for="name" class="text-black fw-medium fs-14">Name</label>
+                                        <input type="text" id="name" name="name"
+                                            class="w-100 input-text border-0 @error('name') is-invalid @enderror"
+                                            data-value="name">
+                                    </div>
+                                </div>
+                                <div class="pt-2 w-100">
+                                    <div class="input-text-wrapper w-100 mb-3">
+                                        <label for="rating" class="text-black fw-medium fs-14">Rating</label>
+                                        <input type="text" id="rating" name="rating"
+                                            class="w-100 input-text border-0 @error('rating') is-invalid @enderror"
+                                            data-value="rating">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pt-2 w-100">
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <label for="location" class="text-black fw-medium fs-14">Location</label>
+                                    <input type="text" id="location" name="location"
+                                        class="w-100 input-text border-0 @error('location') is-invalid @enderror"
+                                        data-value="location">
+                                </div>
+                            </div>
+                            <div class="w-100">
+                                <div class="input-text-wrapper w-100 mb-3">
+                                    <input type="hidden" name="oldImage" data-value="oldImage">
+                                    <p class="text-black fw-medium fs-14">Image</p>
+                                    <div class="d-flex flex-row align-items-end gap-2">
+                                        <img src="" alt="your image" class="tag-image-edit img-preview"
+                                            data-value="image" />
+                                        <input type='file' class="input-file-edit @error('image') is-invalid @enderror"
+                                            size="150" name="image">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wrapper d-flex gap-3">
+                                <div class="w-100">
+                                    <div class="input-text-wrapper w-100 mb-3">
+                                        <label for="history_1" class="text-black fw-medium fs-14">History Paragraph
+                                            1</label>
+                                        <textarea type="text" id="history_1" name="history_1"
+                                            class="w-100 input-text border-0 @error('history_1') is-invalid @enderror" data-value="history_1"
+                                            rows="3"></textarea>
+                                    </div>
+                                </div>
+                                <div class="w-100">
+                                    <div class="input-text-wrapper w-100 mb-3">
+                                        <label for="history_2" class="text-black fw-medium fs-14">History Paragraph
+                                            2</label>
+                                        <textarea type="text" id="history_2" name="history_2"
+                                            class="w-100 input-text border-0 @error('history_2') is-invalid @enderror" data-value="history_2"
+                                            rows="3"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-row justify-content-center gap-2 pt-4">
+                            <button type="button" class="btn btn-dark fs-15" data-bs-dismiss="modal">Cancel
+                                Edit</button>
+                            <button type="submit" class="btn btn-color fs-15">Save Changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <form id="delete-modal" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="modal fade" id="DeleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body p-5">
+                        <div class="text-center fs-3 fw-medium title-font">Delete Destination</div>
+                        <div class="pt-3 text-center"> Do you really want to delete these destination? This process cannot
+                            be
+                            undone.
+                        </div>
+                        <div class="d-flex flex-row justify-content-center gap-2 pt-4">
+                            <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Delete</button>
+                            <button type="submit" class="btn btn-color">Delete Destination</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form> --}}
+
+    <script src="{{ asset('assets/js/jquery-3.5.1.js') }}"></script>
+    <script src="https://kit.fontawesome.com/9e88c62f38.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    <script>
+        $(document).on('click', '[data-bs-target="#EditModal"]', function() {
+            let id = $(this).data('id');
+            $('#edit-modal').attr('action', '/admin/destination/edit/' + id);
+            $.ajax({
+                type: 'get',
+                url: '/admin/destination/edit/' + id,
+                success: function(data) {
+                    $('[data-value="name"]').val(data.name);
+                    $('[data-value="rating"]').val(data.rating);
+                    $('[data-value="location"]').val(data.location);
+                    $('[data-value="oldImage"]').val(data.image);
+                    $('[data-value="image"]').attr("src", "/storage/" + data.image);
+                    $('[data-value="history_1"]').val(data.history_1);
+                    $('[data-value="history_2"]').val(data.history_2);
+                }
+            });
+        });
+
+        $(document).on('click', '[data-bs-target="#DeleteModal"]', function() {
+            let id = $(this).data('id');
+            $('#delete-modal').attr('action', '/admin/destination/delete/' + id);
+        });
+
+        const tagImage = document.querySelector('.tag-image');
+        const inputImage = document.querySelector('.input-file');
+        const tagImageEdit = document.querySelector('.tag-image-edit');
+        const inputImageEdit = document.querySelector('.input-file-edit');
+
+        inputImage.addEventListener('change', function() {
+            tagImage.src = URL.createObjectURL(inputImage.files[0]);
+        });
+
+        inputImageEdit.addEventListener('change', function() {
+            tagImageEdit.src = URL.createObjectURL(inputImageEdit.files[0]);
+        });
+
+        const btnHamburger = document.querySelector('.fa-bars');
+        const sidebar = document.querySelector('.sidebar-wrapper');
+        const body = document.querySelector('body');
+
+        btnHamburger.addEventListener('click', function() {
+            btnHamburger.classList.toggle('active');
+            sidebar.classList.toggle('active');
+        });
+    </script>
+@endsection
